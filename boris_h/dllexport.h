9d103150a9b39b0827ff9c4281701de76d596c48
1024sparrow <1024sparrow@gmail.com>
2019-07-31 04:56:46 +0300

    code upload

---
+++ boris_h/dllexport.h
@@ -0,0 +1,22 @@
+/**********************************************************************************
+ ** Описание: Макросы для экспорта-импорта функций в Windows
+ ** Автор:  Таланов Э.В.
+ ** Дата: 
+ **
+ ** 
+ *********************************************************************************/
+
+#ifdef LIBAPP_API
+#   undef LIBAPP_API
+#endif
+
+#ifdef _WIN32
+#   ifdef  LIBAPP_EXPORTS
+#       define LIBAPP_API __declspec(dllexport)
+#   else
+#       define LIBAPP_API __declspec(dllimport)
+#   endif
+#else
+#   define LIBAPP_API
+#endif
+
